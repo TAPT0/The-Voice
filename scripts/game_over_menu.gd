cf397748078490e367521452e95630bcdf5eedaf
aahil <moominafarash@gmail.com>
2026-03-12 01:31:31 +0530

    .

--- scripts/game_over_menu.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-
-
-func _on_restart_button_pressed():
-	get_tree().reload_current_scene()
-
-func _on_exit_button_pressed():
-	get_tree().quit()
--- scripts/win_screen.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-func _on_play_again_button_pressed():
-	get_tree().paused=false
-	get_tree().reload_current_scene()
-
-
-func _on_quit_button_pressed():
-	get_tree().quit()
